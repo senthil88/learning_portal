daa5051121e74bed3aca32ed4df0b5e2acf0f07e
senthilkumar <rask.kumar@gmail.com>
2014-05-12 11:53:18 +0530

    Move course seeds to seeds.rb

--- app/models/course.rb
+++ app/models/course.rb
@@ -16,43 +16,4 @@ class Course < ActiveRecord::Base
   # Scopes
 
   # Callbacks
-
-
-  def self.create_from_yaml!
-    courses_yml = Rails.root.join('db', 'seeds', 'courses.yml')
-
-    if File.exists? courses_yml
-      YAML.load(File.read(courses_yml)).each do |course_attribute|
-        course_attribute.symbolize_keys!
-
-        # Create courses
-        course = Course.first_or_initialize(name: course_attribute[:name]).tap do |course|
-          course.image_url = course_attribute[:image_url]
-          course.description = course_attribute[:description]
-        end.save!
-
-        # Create Tutorials for course
-        course_attribute[:tutorials].each do |tutorial_attribute|
-          tutorial_attribute.symbolize_keys!
-          tutorial = course.tutorials.first_or_initialize(title: tutorial_attribute[:title]).tap do |tutorial|
-            tutorial.image_url = tutorial_attribute[:image_url]
-            tutorial.description = tutorial_attribute[:description]
-          end.save!
-
-          # Create Tag for tutorial
-          tutorial_attribute[:tags].each {|tag_attribute| tutorial.tags.find_or_create_by! name: tag_attribute["name"]}
-
-          # Create Videos for tutorial
-          tutorial_attribute[:videos].each do |video_attribute|
-            video_attribute.symbolize_keys!
-            tutorial.videos.first_or_initialize(title: video_attribute[:title]).tap do |video|
-              video.video_url = video_attribute[:video_url]
-              video.image_url = video_attribute[:image_url]
-              video.description = video_attribute[:description]
-            end.save!
-          end
-        end
-      end
-    end
-  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,33 @@ def seed(table_name)
     end
   end
 end
+
+seed :courses do |course_attribute|
+  # Create courses
+  course = Course.find_or_create_by! name: course_attribute[:name] do |course|
+    course.image_url = course_attribute[:image_url]
+    course.description = course_attribute[:description]
+  end
+
+  # Create Tutorials for course
+  course_attribute[:tutorials].each do |tutorial_attribute|
+    tutorial_attribute.symbolize_keys!
+    tutorial = course.tutorials.find_or_create_by! title: tutorial_attribute[:title] do |tutorial|
+      tutorial.image_url = tutorial_attribute[:image_url]
+      tutorial.description = tutorial_attribute[:description]
+    end
+
+    # Create Tag for tutorial
+    tutorial_attribute[:tags].each {|tag_attribute| tutorial.tags << Tag.find_or_create_by!(name: tag_attribute["name"])}
+
+    # Create Videos for tutorial
+    tutorial_attribute[:videos].each do |video_attribute|
+      video_attribute.symbolize_keys!
+      tutorial.videos.first_or_initialize(title: video_attribute[:title]).tap do |video|
+        video.video_url = video_attribute[:video_url]
+        video.image_url = video_attribute[:image_url]
+        video.description = video_attribute[:description]
+      end.save!
+    end
+  end
+end
