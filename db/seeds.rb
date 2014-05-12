6395c10ed6d013fd68e608b228829a67201962e2
senthilkumar <rask.kumar@gmail.com>
2014-05-12 11:51:10 +0530

    Add seed method to support all table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,11 @@
-Course.create_from_yaml!
+def seed(table_name)
+  yaml_file = Rails.root.join('db', 'seeds', "#{table_name}.yml")
+
+  if File.exists? yaml_file
+    YAML.load(File.read(yaml_file)).each do |attributes|
+      attributes.symbolize_keys!
+
+      yield attributes
+    end
+  end
+end
