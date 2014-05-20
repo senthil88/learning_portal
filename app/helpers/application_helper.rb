ea38f83a6dcf49ab40397d6e9d2db658907bce74
senthilkumar <rask.kumar@gmail.com>
2014-05-20 12:52:05 +0530

    Set default image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,5 @@
 module ApplicationHelper
-  def set_image(image_url, options = {})
-    class_name = options[:class].present? ? options[:class] : ''
-    capture_haml do
-      unless image_url.empty?
-        haml_tag :img, class: class_name, src: image_url
-      else
-        haml_tag :img, class: class_name, src: '/assets/no_image.gif'
-      end
-    end
+  def set_image(image_url)
+    image_url.present? ? image_url : '/assets/no_image.gif'
   end
 end
