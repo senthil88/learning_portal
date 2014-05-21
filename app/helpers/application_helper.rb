45ff1627508564a6182cac186568bdce939938fe
senthilkumar <rask.kumar@gmail.com>
2014-05-21 14:05:34 +0530

    Use asset_path instead of normal url in css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def set_image(image_url)
-    image_url.present? ? image_url : '/assets/no_image.gif'
+    image_url.present? ? image_url : asset_path('no_image.gif')
   end
 end
